fb84b2b4eca5b515c5dd25fd9aca17f8eea8e244
Sourabh Choure <sourabhchoure98@gmail.com>
2022-08-20 23:09:31 -0500

    [PF] [UI] Skills section

--- src/intro_view/Skills.js
+++ src/intro_view/Skills.js
@@ -11,7 +11,99 @@ const Skills = () => {
         <Grid container direction="column" justify="center" rowSpacing={2}>
           <Grid item>
             <Box sx={{ fontWeight: 700, fontSize: "1.6rem" }}>Skill Sets</Box>
-            
+            <Box sx={{}}>Programming Languages</Box>
+            <img
+              alt="C"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"
+            />
+
+            <img
+              alt="C++"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
+            />
+
+            <img
+              alt="php"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"
+            />
+
+            <img
+              alt="java"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg"
+            />
+
+            <img
+              alt="go"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg"
+            />
+
+            <img
+              alt="python"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg"
+            />
+
+            <img
+              alt="ruby"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original-wordmark.svg"
+            />
+
+            <img
+              alt="bash"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg"
+            />
+
+            <Box sx={{}}>Framework and Tools</Box>
+
+            <img
+              alt="react"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+            />
+
+            <img
+              alt="nodejs"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg"
+            />
+
+            <img
+              alt="graphql"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain-wordmark.svg"
+            />
+
+            <img
+              alt="redux"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
+            />
+
+            <img
+              alt="linux"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg"
+            />
+
+            <img
+              alt="mysql"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
+            />
+
+            <Box sx={{}}>Cloud Platfdorm</Box>
+            <img
+              alt="mysql"
+              height="50px"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
+            />
           </Grid>
         </Grid>
       </CardContent>
