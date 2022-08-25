72d88ac242c8ce9e66df0eea6af997162db46be8
Sourabh Choure <sourabhchoure98@gmail.com>
2022-08-25 18:51:44 -0500

    [PF] [UI] NavBar Placeholder

--- src/App.js
+++ src/App.js
@@ -8,11 +8,66 @@ import Box from "@mui/material/Box";
 import Alert from "@mui/material/Alert";
 import IconButton from "@mui/material/IconButton";
 import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import { styled, useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const MiniDrawer = styled(Drawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
 function AlertBox() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("md"));
+
   const [openAlert, setOpenAlert] = React.useState(true);
   return (
-    <Box sx={{ width: "100%", position: "absolute" }} p={0}>
+    <Box sx={{ position: "absolute", width: "100%" }} p={0}>
       <Collapse in={openAlert}>
         <Alert
           severity="info"
@@ -28,22 +83,35 @@ function AlertBox() {
               <CloseIcon fontSize="inherit" />
             </IconButton>
           }
-          sx={{ mb: 2 }}
         >
-          <strong>This website is still under development! Here be dragons.</strong>
+          <strong>
+            This website is still under development! Here be dragons.
+          </strong>
         </Alert>
       </Collapse>
     </Box>
   );
 }
 function App() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("md"));
+  const [open, setOpen] = React.useState(false);
   return (
-    <>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AlertBox />
-      <Intro />
-      <WorkEXP />
-    </>
+      {/* {matches ? (
+        <MiniDrawer variant="permanent" open={open}>
+          hi
+        </MiniDrawer>
+      ) : (
+        <></>
+      )} */}
+      <Box flexGrow={1}>
+        <AlertBox />
+        <Intro />
+        <WorkEXP />
+      </Box>
+    </Box>
   );
 }
 
